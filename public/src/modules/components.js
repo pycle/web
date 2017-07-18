d89058f4ae2f1ea1a68f4dcd917f613e07b55317
Baris Usakli <barisusakli@gmail.com>
2017-07-18 12:30:09 -0400

    add data-component support

--- public/src/modules/components.js
+++ public/src/modules/components.js
@@ -6,52 +6,52 @@ define('components', function () {
 	components.core = {
 		'topic/teaser': function (tid) {
 			if (tid) {
-				return $('[component="category/topic"][data-tid="' + tid + '"] [component="topic/teaser"]');
+				var cmp = getComponent('category/topic', 'tid', tid);
+				return findInComponent(cmp, 'topic/teaser');
 			}
-			return $('[component="topic/teaser"]');
+			return getComponent('topic/teaser');
 		},
 		topic: function (name, value) {
-			return $('[component="topic"][data-' + name + '="' + value + '"]');
+			return getComponent('topic', name, value);
 		},
 		post: function (name, value) {
-			return $('[component="post"][data-' + name + '="' + value + '"]');
+			return getComponent('post', name, value);
 		},
 		'post/content': function (pid) {
-			return components.core.post('pid', pid).find('[component="post/content"]');
+			return findInComponent(components.core.post('pid', pid), 'post/content');
 		},
 		'post/header': function (pid) {
-			return components.core.post('pid', pid).find('[component="post/header"]');
+			return findInComponent(components.core.post('pid', pid), 'post/header');
 		},
 		'post/anchor': function (index) {
-			return components.core.post('index', index).find('[component="post/anchor"]');
+			return findInComponent(components.core.post('index', index), 'post/anchor');
 		},
 		'post/vote-count': function (pid) {
-			return components.core.post('pid', pid).find('[component="post/vote-count"]');
+			return findInComponent(components.core.post('pid', pid), 'post/vote-count');
 		},
 		'post/bookmark-count': function (pid) {
-			return components.core.post('pid', pid).find('[component="post/bookmark-count"]');
+			return findInComponent(components.core.post('pid', pid), 'post/bookmark-count');
 		},
 
 		'user/postcount': function (uid) {
-			return $('[component="user/postcount"][data-uid="' + uid + '"]');
+			return getComponent('user/postcount', 'uid', uid);
 		},
 		'user/reputation': function (uid) {
-			return $('[component="user/reputation"][data-uid="' + uid + '"]');
+			return getComponent('user/reputation', 'uid', uid);
 		},
 
 		'category/topic': function (name, value) {
-			return $('[component="category/topic"][data-' + name + '="' + value + '"]');
+			return getComponent('category/topic', name, value);
 		},
-
 		'categories/category': function (name, value) {
-			return $('[component="categories/category"][data-' + name + '="' + value + '"]');
+			return getComponent('categories/category', name, value);
 		},
 
 		'chat/message': function (messageId) {
-			return $('[component="chat/message"][data-mid="' + messageId + '"]');
+			return getComponent('chat/message', 'mid', messageId);
 		},
 		'chat/message/body': function (messageId) {
-			return $('[component="chat/message"][data-mid="' + messageId + '"] [component="chat/message/body"]');
+			return findInComponent(getComponent('chat/message', 'mid', messageId), 'chat/message/body');
 		},
 	};
 
@@ -61,8 +61,31 @@ define('components', function () {
 		if (components.core[arguments[0]] && args.length) {
 			return components.core[arguments[0]].apply(this, args);
 		}
-		return $('[component="' + arguments[0] + '"]');
+
+		return getComponent(arguments[0]);
 	};
+
+	function getComponent(componentName, dataName, dataValue) {
+		var dataSelector = '';
+		if (dataName && dataValue) {
+			dataSelector = '[data-' + dataName + '="' + dataValue + '"]';
+		}
+		var cmp = $('[data-component="' + componentName + '"]' + dataSelector);
+		if (!cmp.length) {
+			// backwards compatibility
+			cmp = $('[component="' + componentName + '"]' + dataSelector);
+		}
+		return cmp;
+	}
+
+	function findInComponent(cmp, componentName) {
+		var child = cmp.find('[data-component="' + componentName + '"]');
+		if (!child.length) {
+			// backwards compatibility
+			child = cmp.find('[component="' + componentName + '"]');
+		}
+		return child;
+	}
 
 	return components;
 });
